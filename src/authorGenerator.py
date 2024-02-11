670b7569c722f3bea332c31daa2864cf48bdb557
Kenneth John <kensplanet@yahoo.com>
2024-02-11 11:42:43 -0500

    Update authorGenerator.py

--- src/authorGenerator.py
+++ src/authorGenerator.py
@@ -1,12 +1,12 @@
 import random
 from time import time
 authors = [
-    ("Napoleon Bonaparte", 'images\\napoleon-bonaparte.jpg'),
-    ("Marcus Aurelius", 'images\\marcus-aurelius.jpg'),
-    ("Andrew Carnegie", 'images\\andrew-carnegie.jpg'),
-    ("Henry Ford", 'images\\henry-ford.jpg'),
-    ("Thomas Edison", 'images\\thomas-edison.jpg'),
-    ("Walt Disney", 'images\\walt-disney.jpg')
+    ("Napoleon Bonaparte", 'images/napoleon-bonaparte.jpg'),
+    ("Marcus Aurelius", 'images/marcus-aurelius.jpg'),
+    ("Andrew Carnegie", 'images/andrew-carnegie.jpg'),
+    ("Henry Ford", 'images/henry-ford.jpg'),
+    ("Thomas Edison", 'images/thomas-edison.jpg'),
+    ("Walt Disney", 'images/walt-disney.jpg')
 ]
 
 
